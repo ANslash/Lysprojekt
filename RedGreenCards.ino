e0d5cf615097a062ed569de826af260d439ce2b9
ANslash <75243928+ANslash@users.noreply.github.com>
2020-11-30 15:00:30 +0100

    Add files via upload
    Cards working
    brightness control working

--- RedGreenCards.ino
+++ RedGreenCards.ino
@@ -1,6 +1,7 @@
 #include <NodeHue.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <SR04.h>
 
 // SPECIFY FOR YOUR SETUP
 #define IP "192.168.0.183" // Ip-adress of Philips Hue Bridge
@@ -10,13 +11,23 @@
 #define red 3
 #define SS_PIN 10
 #define RST_PIN 9
+#define TRIG_PIN 6
+#define ECHO_PIN 7
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+long a;
 
 NodeHue hue(IP, &Serial); // Define NodeHue
 long color = 0;
 int sat = 0;
 int bri = 0;
 int scan = 0;
+int change;
+
+boolean card1 = false;
+boolean card2 = false;
+boolean card3 = false;
 
 void setup(){
   hue.connect(PASS); // Setup serial connection
@@ -29,6 +40,7 @@ void setup(){
   // RFID READER
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
+
 }
 
 void cardScan() {
@@ -55,53 +67,103 @@ void cardScan() {
   }
   delay(100);
   content.toUpperCase();
-  if (content.substring(1) == "20 1C 8B 7C") //change here the UID of the card/cards that you want to give access
+  if (content.substring(1) == "20 1C 8B 7C" && card1 == false) //change here the UID of the card/cards that you want to give access
   {
     hue.turnOn(ID,true);
     color += 60000;
     sat += 255;
     bri += 255;
-    scan++; 
+    scan++;
+    card1 = true;
+    change = 1;
   }
+  else if (content.substring(1) == "20 1C 8B 7C" && card1 == true){
+    color -= 60000;
+    sat -= 255;
+    bri -= 255;
+    scan--;
+    card1 = false;
+    }
+  
 
-  if (content.substring(1) == "10 3F 8C 7C") //change here the UID of the card/cards that you want to give access
+  if (content.substring(1) == "10 3F 8C 7C" && card2 == false) //change here the UID of the card/cards that you want to give access
   {
     hue.turnOn(ID,true);
     color += 30000;
     sat += 100;
     bri += 50;
-    scan++; 
+    scan++;
+    card2 = true;
+    change = 1;
   }
+  else if (content.substring(1) == "10 3F 8C 7C" && card2 == true) {
+    color -= 30000;
+    sat -= 100;
+    bri -= 50;
+    scan--;
+    card2 = false;
+    }
 
-  if (content.substring(1) == "4B B9 2A 0B") //change here the UID of the card/cards that you want to give access
+  if (content.substring(1) == "4B B9 2A 0B" && card3 == false) //change here the UID of the card/cards that you want to give access
   {
     hue.turnOn(ID,true);
     color += 500;
-    sat += 255;
-    bri += 255;
-    scan++; 
+    sat += 200;
+    bri += 200;
+    scan++;
+    card3 = true;
+    change = 1;
   }
-  delay(50);
+  else if (content.substring(1) == "4B B9 2A 0B" && card3 == true) {
+    color -= 500;
+    sat -= 200;
+    bri -= 200;
+    scan--;
+    card3 = false;
+    }
+
 
   if (content.substring(1) == "04 AB 8B 72 97 3C 80") //change here the UID of the card/cards that you want to give access
   {
     color = 0;
     sat = 0;
     bri = 0;
-    scan = 0; 
+    scan = 0;
+
+    card1 = false;
+    card2 = false;
+    card3 = false;
+    
     hue.turnOn(ID,false);
   }
 }
 
 void setColor() {
   hue.setHueSat(ID, color/scan, sat/scan);
+  //hue.setBri(ID,bri/scan);
+}
+
+void setNewColor() {
+  hue.setHueSat(ID, color/scan, sat/scan);
   hue.setBri(ID,bri/scan);
 }
 
 void loop(){
     cardScan();
-    if (scan != 0) {
-    setColor();
+    if (scan > 0 && change == 1) {
+      setNewColor();
     }
-    delay(50);
+    else if (scan > 0) {
+      setColor();
+      }
+    else if (scan == 0) {
+      hue.turnOn(ID, false);
+      }
+    long dis = sr04.Distance();
+    if (0 <= dis && dis <= 25.5) {
+      hue.setBri(ID, (dis * 10));
+      Serial.println(dis*10);
+      }
+    change = 0;
+    delay(300);
 } 
