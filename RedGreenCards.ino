e941529ddf3b44a88d3d8e8bbf86259986b45677
ANslash <75243928+ANslash@users.noreply.github.com>
2020-11-30 11:58:36 +0100

    Add files via upload

--- RedGreenCards.ino
+++ RedGreenCards.ino
@@ -8,16 +8,19 @@
 #define ID 4 // lamp ID
 #define green 2
 #define red 3
-#define SS_PIN 9
-#define RST_PIN 8
+#define SS_PIN 10
+#define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
 NodeHue hue(IP, &Serial); // Define NodeHue
+long color = 0;
+int sat = 0;
+int bri = 0;
+int scan = 0;
 
 void setup(){
   hue.connect(PASS); // Setup serial connection
   Serial.begin(115200); // Setup Serial. MUST BE 115200
-  Serial1.begin(9600); //
   hue.turnOn(ID,true); // Turn on hue (not necessary)
 
   pinMode(red, OUTPUT);
@@ -28,7 +31,7 @@ void setup(){
   mfrc522.PCD_Init();   // Initiate MFRC522
 }
 
-void loop(){
+void cardScan() {
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
@@ -40,32 +43,65 @@ void loop(){
     return;
   }
   //Show UID on serial monitor
-  Serial1.print("UID tag :");
+  Serial.print("UID tag :");
   String content= "";
   byte letter;
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
-     Serial1.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial1.print(mfrc522.uid.uidByte[i], HEX);
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.println(mfrc522.uid.uidByte[i], HEX);
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
-  
+  delay(100);
   content.toUpperCase();
   if (content.substring(1) == "20 1C 8B 7C") //change here the UID of the card/cards that you want to give access
   {
-    digitalWrite(green, HIGH);
-    hue.setRGB(ID, 0, 255, 0 );
-    delay(3000);
-    digitalWrite(green, LOW);
-    hue.setRGB(ID, 0, 0, 250 );
+    hue.turnOn(ID,true);
+    color += 60000;
+    sat += 255;
+    bri += 255;
+    scan++; 
   }
- 
- else   {
-    digitalWrite(red, HIGH);
-    hue.setRGB(ID, 255, 0, 0 );
-    delay(3000);
-    digitalWrite(red, LOW);
-    hue.setRGB(ID, 0, 0, 250 );
+
+  if (content.substring(1) == "10 3F 8C 7C") //change here the UID of the card/cards that you want to give access
+  {
+    hue.turnOn(ID,true);
+    color += 30000;
+    sat += 100;
+    bri += 50;
+    scan++; 
   }
+
+  if (content.substring(1) == "4B B9 2A 0B") //change here the UID of the card/cards that you want to give access
+  {
+    hue.turnOn(ID,true);
+    color += 500;
+    sat += 255;
+    bri += 255;
+    scan++; 
+  }
+  delay(50);
+
+  if (content.substring(1) == "04 AB 8B 72 97 3C 80") //change here the UID of the card/cards that you want to give access
+  {
+    color = 0;
+    sat = 0;
+    bri = 0;
+    scan = 0; 
+    hue.turnOn(ID,false);
+  }
+}
+
+void setColor() {
+  hue.setHueSat(ID, color/scan, sat/scan);
+  hue.setBri(ID,bri/scan);
+}
+
+void loop(){
+    cardScan();
+    if (scan != 0) {
+    setColor();
+    }
+    delay(50);
 } 
